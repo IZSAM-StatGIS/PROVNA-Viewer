f176ad08daa75d85ed01ecf482ec55e479af09c9
Alessio Di Lorenzo <a.dilorenzo@izs.intra>
2026-01-19 11:53:57 +0100

    Aggiunta la colormap a 94 classi

---
+++ assets/ct94.js
@@ -0,0 +1,566 @@
+export default [
+    {
+        "value": 1,
+        "target": {
+            "color": "#3496A6"
+        }
+    },
+    {
+        "value": 2,
+        "target": {
+            "color": "#86263F"
+        }
+    },
+    {
+        "value": 3,
+        "target": {
+            "color": "#23D6A2"
+        }
+    },
+    {
+        "value": 4,
+        "target": {
+            "color": "#35B38C"
+        }
+    },
+    {
+        "value": 5,
+        "target": {
+            "color": "#D11A22"
+        }
+    },
+    {
+        "value": 6,
+        "target": {
+            "color": "#596869"
+        }
+    },
+    {
+        "value": 7,
+        "target": {
+            "color": "#4B8A96"
+        }
+    },
+    {
+        "value": 8,
+        "target": {
+            "color": "#3FBEA7"
+        }
+    },
+    {
+        "value": 9,
+        "target": {
+            "color": "#31C3BF"
+        }
+    },
+    {
+        "value": 10,
+        "target": {
+            "color": "#12D7CC"
+        }
+    },
+    {
+        "value": 11,
+        "target": {
+            "color": "#BC1E07"
+        }
+    },
+    {
+        "value": 12,
+        "target": {
+            "color": "#5B8059"
+        }
+    },
+    {
+        "value": 13,
+        "target": {
+            "color": "#3A9696"
+        }
+    },
+    {
+        "value": 14,
+        "target": {
+            "color": "#518EB2"
+        }
+    },
+    {
+        "value": 15,
+        "target": {
+            "color": "#09ECB1"
+        }
+    },
+    {
+        "value": 16,
+        "target": {
+            "color": "#5647A6"
+        }
+    },
+    {
+        "value": 17,
+        "target": {
+            "color": "#06E4D5"
+        }
+    },
+    {
+        "value": 18,
+        "target": {
+            "color": "#E41705"
+        }
+    },
+    {
+        "value": 19,
+        "target": {
+            "color": "#449EB6"
+        }
+    },
+    {
+        "value": 20,
+        "target": {
+            "color": "#12DFBD"
+        }
+    },
+    {
+        "value": 21,
+        "target": {
+            "color": "#39B4B1"
+        }
+    },
+    {
+        "value": 22,
+        "target": {
+            "color": "#E71823"
+        }
+    },
+    {
+        "value": 23,
+        "target": {
+            "color": "#38C5E5"
+        }
+    },
+    {
+        "value": 24,
+        "target": {
+            "color": "#3BA1BE"
+        }
+    },
+    {
+        "value": 25,
+        "target": {
+            "color": "#3DC7CA"
+        }
+    },
+    {
+        "value": 26,
+        "target": {
+            "color": "#707D99"
+        }
+    },
+    {
+        "value": 27,
+        "target": {
+            "color": "#5D63A0"
+        }
+    },
+    {
+        "value": 28,
+        "target": {
+            "color": "#3AB6CA"
+        }
+    },
+    {
+        "value": 29,
+        "target": {
+            "color": "#3AAFD2"
+        }
+    },
+    {
+        "value": 30,
+        "target": {
+            "color": "#3AD8CE"
+        }
+    },
+    {
+        "value": 31,
+        "target": {
+            "color": "#40C4AB"
+        }
+    },
+    {
+        "value": 32,
+        "target": {
+            "color": "#34BA9A"
+        }
+    },
+    {
+        "value": 33,
+        "target": {
+            "color": "#BE1F49"
+        }
+    },
+    {
+        "value": 34,
+        "target": {
+            "color": "#3FB5AF"
+        }
+    },
+    {
+        "value": 35,
+        "target": {
+            "color": "#BA6737"
+        }
+    },
+    {
+        "value": 36,
+        "target": {
+            "color": "#955374"
+        }
+    },
+    {
+        "value": 37,
+        "target": {
+            "color": "#5B7590"
+        }
+    },
+    {
+        "value": 38,
+        "target": {
+            "color": "#42DCC4"
+        }
+    },
+    {
+        "value": 39,
+        "target": {
+            "color": "#39E6B3"
+        }
+    },
+    {
+        "value": 40,
+        "target": {
+            "color": "#84C92E"
+        }
+    },
+    {
+        "value": 41,
+        "target": {
+            "color": "#75A380"
+        }
+    },
+    {
+        "value": 42,
+        "target": {
+            "color": "#83C0BC"
+        }
+    },
+    {
+        "value": 43,
+        "target": {
+            "color": "#3BB0BD"
+        }
+    },
+    {
+        "value": 44,
+        "target": {
+            "color": "#5AC8B2"
+        }
+    },
+    {
+        "value": 45,
+        "target": {
+            "color": "#4FB3C7"
+        }
+    },
+    {
+        "value": 46,
+        "target": {
+            "color": "#46BEE3"
+        }
+    },
+    {
+        "value": 47,
+        "target": {
+            "color": "#D31429"
+        }
+    },
+    {
+        "value": 48,
+        "target": {
+            "color": "#DB1D40"
+        }
+    },
+    {
+        "value": 49,
+        "target": {
+            "color": "#7FA7BC"
+        }
+    },
+    {
+        "value": 50,
+        "target": {
+            "color": "#C05149"
+        }
+    },
+    {
+        "value": 51,
+        "target": {
+            "color": "#CF2173"
+        }
+    },
+    {
+        "value": 52,
+        "target": {
+            "color": "#58C0EC"
+        }
+    },
+    {
+        "value": 53,
+        "target": {
+            "color": "#899F7F"
+        }
+    },
+    {
+        "value": 54,
+        "target": {
+            "color": "#DF1938"
+        }
+    },
+    {
+        "value": 55,
+        "target": {
+            "color": "#9764AB"
+        }
+    },
+    {
+        "value": 56,
+        "target": {
+            "color": "#67BFBF"
+        }
+    },
+    {
+        "value": 57,
+        "target": {
+            "color": "#CD1C35"
+        }
+    },
+    {
+        "value": 58,
+        "target": {
+            "color": "#7B90A5"
+        }
+    },
+    {
+        "value": 59,
+        "target": {
+            "color": "#7385CE"
+        }
+    },
+    {
+        "value": 60,
+        "target": {
+            "color": "#9F8389"
+        }
+    },
+    {
+        "value": 61,
+        "target": {
+            "color": "#DB3141"
+        }
+    },
+    {
+        "value": 62,
+        "target": {
+            "color": "#B02771"
+        }
+    },
+    {
+        "value": 63,
+        "target": {
+            "color": "#9D297C"
+        }
+    },
+    {
+        "value": 64,
+        "target": {
+            "color": "#52B0A4"
+        }
+    },
+    {
+        "value": 65,
+        "target": {
+            "color": "#67699D"
+        }
+    },
+    {
+        "value": 66,
+        "target": {
+            "color": "#B22062"
+        }
+    },
+    {
+        "value": 67,
+        "target": {
+            "color": "#5A5D87"
+        }
+    },
+    {
+        "value": 68,
+        "target": {
+            "color": "#68A88E"
+        }
+    },
+    {
+        "value": 69,
+        "target": {
+            "color": "#B71F4E"
+        }
+    },
+    {
+        "value": 70,
+        "target": {
+            "color": "#D71E35"
+        }
+    },
+    {
+        "value": 71,
+        "target": {
+            "color": "#BF213E"
+        }
+    },
+    {
+        "value": 72,
+        "target": {
+            "color": "#7B2F74"
+        }
+    },
+    {
+        "value": 73,
+        "target": {
+            "color": "#49AB97"
+        }
+    },
+    {
+        "value": 74,
+        "target": {
+            "color": "#7F4785"
+        }
+    },
+    {
+        "value": 75,
+        "target": {
+            "color": "#BE1E29"
+        }
+    },
+    {
+        "value": 76,
+        "target": {
+            "color": "#603277"
+        }
+    },
+    {
+        "value": 77,
+        "target": {
+            "color": "#A0225B"
+        }
+    },
+    {
+        "value": 78,
+        "target": {
+            "color": "#47A49F"
+        }
+    },
+    {
+        "value": 79,
+        "target": {
+            "color": "#3F8383"
+        }
+    },
+    {
+        "value": 80,
+        "target": {
+            "color": "#92205E"
+        }
+    },
+    {
+        "value": 81,
+        "target": {
+            "color": "#4D6E73"
+        }
+    },
+    {
+        "value": 82,
+        "target": {
+            "color": "#723A75"
+        }
+    },
+    {
+        "value": 83,
+        "target": {
+            "color": "#B71F2E"
+        }
+    },
+    {
+        "value": 84,
+        "target": {
+            "color": "#1EC397"
+        }
+    },
+    {
+        "value": 85,
+        "target": {
+            "color": "#4678AE"
+        }
+    },
+    {
+        "value": 86,
+        "target": {
+            "color": "#1FD0C8"
+        }
+    },
+    {
+        "value": 87,
+        "target": {
+            "color": "#7F2B5B"
+        }
+    },
+    {
+        "value": 88,
+        "target": {
+            "color": "#2DAB8F"
+        }
+    },
+    {
+        "value": 89,
+        "target": {
+            "color": "#09D3A2"
+        }
+    },
+    {
+        "value": 90,
+        "target": {
+            "color": "#A2242B"
+        }
+    },
+    {
+        "value": 91,
+        "target": {
+            "color": "#54667F"
+        }
+    },
+    {
+        "value": 92,
+        "target": {
+            "color": "#49A6AC"
+        }
+    },
+    {
+        "value": 93,
+        "target": {
+            "color": "#0FCCAE"
+        }
+    },
+    {
+        "value": 94,
+        "target": {
+            "color": "#16DD9C"
+        }
+    }
+]
